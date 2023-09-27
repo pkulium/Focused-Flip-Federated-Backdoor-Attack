b3ec0ba6aa93d2a00a3b3a5f35e5b1338e04467f
mliu <pkulium@gmail.com>
2023-09-27 11:09:03 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Bases.py

--- Bases.py
+++ Bases.py
@@ -97,11 +97,11 @@ class FederatedBackdoorExperiment:
                     client.idle()
                 else:
                     client.handcraft(self.task)
-                    client.train(self.task)
-                    # if epoch != self.params.n_epochs - 1:
-                    #     client.train(self.task)
-                    # else:
-                    #     client.train_mask(self.task)
+                    # client.train(self.task)
+                    if epoch != self.params.n_epochs - 1:
+                        client.train(self.task)
+                    else:
+                        client.train_mask(self.task)
             self.server.aggregate_global_model(self.clients, chosen_ids, None)
             print('Round {}: FedAvg Testing'.format(epoch))
             fl_report.record_round_vars(self.test(epoch, backdoor=False))
@@ -110,8 +110,8 @@ class FederatedBackdoorExperiment:
                 saved_name = identifier + "_{}".format(epoch + 1)
                 save_report(fl_report, './{}'.format(saved_name))
             print('-' * 30)
-        torch.save(self.server.global_model.state_dict(), '/work/LAS/wzhang-lab/mingl/code/client_defense/save/toxin_fedavg.th')
-         
+        torch.save(self.server.global_model.state_dict(), f'/work/LAS/wzhang-lab/mingl/code/client_defense/save/{identifier}.th')
+
 
     def finetuning_training(self, identifier=None):
         fl_report.create_record(identifier, checkout=True)
