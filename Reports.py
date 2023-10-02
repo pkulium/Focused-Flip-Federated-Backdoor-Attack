96004263b74863124502bf41e4042a278c16b22b
mliu <pkulium@gmail.com>
2023-10-02 17:54:54 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Reports.py

--- Reports.py
+++ Reports.py
@@ -99,6 +99,6 @@ def load_report(path):
 
 
 import pickle
-with open("/work/LAS/wzhang-lab/mingl/code/client_defense/result-fedavg/naive_fedavg_cifar_resnet18_h1.0_c20_100", 'rb') as f:
+with open("/work/LAS/wzhang-lab/mingl/code/client_defense/result-fedavg/dba_ensemble-distillation_cifar_resnet18_h1.0_c20_100", 'rb') as f:
     b = pickle.load(f)
 print(b)
