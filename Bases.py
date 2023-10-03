4fe457489edbb434b95884ccae3f2e1fcb77dc36
mliu <pkulium@gmail.com>
2023-10-03 12:14:17 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   Bases.py
    	modified:   Server.py
    	modified:   configs/cifar_fed.yaml

--- Bases.py
+++ Bases.py
@@ -41,7 +41,7 @@ class FederatedBackdoorExperiment:
 
         base_model = self.task.build_model()
 
-        base_model.load_state_dict(torch.load('save/ff/naive.th'))
+        # base_model.load_state_dict(torch.load('save/ff/naive.th'))
         base_optimizer = self.task.build_optimizer(base_model)
         splited_dataset = self.task.sample_dirichlet_train_data(params.n_clients)
         server_sample_ids = splited_dataset[params.n_clients]
@@ -91,9 +91,8 @@ class FederatedBackdoorExperiment:
             print('Round {}: FedAvg Training'.format(epoch))
             self.server.broadcast_model_weights(self.clients)
             chosen_ids = self.server.select_participated_clients(fixed_mal=[])
-            chosen_ids = [0   ,12  ,16  ,19  ,7  ,10  ,13  ,18  ,4   ,8]
-            chosen_ids = [0   ,16  ,19  ,7  ,10  ,18  ,4   ,8]
-            chosen_ids = [0]
+            # chosen_ids = [0   ,12  ,16  ,19  ,7  ,10  ,13  ,18  ,4   ,8]
+            # chosen_ids = [0]
             for client in self.clients:
                 client.global_epoch = epoch
                 if client.client_id not in chosen_ids:
@@ -101,7 +100,7 @@ class FederatedBackdoorExperiment:
                 else:
                     client.handcraft(self.task)
                     # client.train(self.task)
-                    if epoch != self.params.n_epochs - 1:
+                    if epoch != self.params.n_epochs:
                         client.train(self.task)
                     else:
                         client.train_mask(self.task)
@@ -113,7 +112,7 @@ class FederatedBackdoorExperiment:
                 saved_name = identifier + "_{}".format(epoch + 1)
                 save_report(fl_report, './{}'.format(saved_name))
             print('-' * 30)
-        # torch.save(self.server.global_model.state_dict(), f'/work/LAS/wzhang-lab/mingl/code/client_defense/save/naive.th')
+        torch.save(self.server.global_model.state_dict(), f'/work/LAS/wzhang-lab/mingl/code/client_defense/save/fedavg_naive.th')
 
 
     def finetuning_training(self, identifier=None):
