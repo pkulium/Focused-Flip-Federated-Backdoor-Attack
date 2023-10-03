14245c23e00424563d9fbeb34e639bca16b4d05d
mliu <pkulium@gmail.com>
2023-10-03 12:16:16 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Client.py

--- Client.py
+++ Client.py
@@ -431,7 +431,7 @@ class Client(Clientbase):
                                            mask_opt=mask_optimizer, noise_opt=noise_optimizer)
         self.mask_scores = get_mask_scores(self.local_model.state_dict())
         save_mask_scores(self.local_model.state_dict(), f'save/mask_values_{self.client_id}_{self.is_malicious}.txt')
-        mask_values = read_data(f'save/ff/mask_values_{self.client_id}_{self.is_malicious}.txt') in ids:
+        mask_values = read_data(f'save/ff/mask_values_{self.client_id}_{self.is_malicious}.txt')
         mask_values = read_data(f'save/ff/mask_values_{self.client_id}.txt')
         mask_values = sorted(mask_values, key=lambda x: float(x[2]))
         print(f'mask_values:{mask_values[0]} - {mask_values[10]}')
