8affc977c2b3b710ec753b411438cb559cfa891d
mliu <pkulium@gmail.com>
2023-10-10 07:50:47 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .DS_Store
    	modified:   Client.py
    	modified:   save/.DS_Store

--- Client.py
+++ Client.py
@@ -434,8 +434,8 @@ class Client(Clientbase):
         mask_values = read_data(f'save/ff/mask_values_{self.client_id}_{self.is_malicious}.txt')
         mask_values = read_data(f'save/ff/mask_values_{self.client_id}.txt')
         mask_values = sorted(mask_values, key=lambda x: float(x[2]))
-        print(f'mask_values:{mask_values[0]} - {mask_values[10]}')
-        prune_by_threshold(self.local_model, mask_values, pruning_max=0.2, pruning_step=0.001)
+        print(f'{self.is_malicious} mask_values:{mask_values[0]} - {mask_values[10]}')
+        prune_by_threshold(self.local_model, mask_values, pruning_max=0.3, pruning_step=0.001)
 
     def handcraft(self, task):
         self.handcraft_rnd = self.handcraft_rnd + 1
