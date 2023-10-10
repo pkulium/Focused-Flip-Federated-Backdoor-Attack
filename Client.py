c2319c3ab237dec5f9fb2751d44c1565a54fe0c8
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-10 17:21:13 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Client.py

--- Client.py
+++ Client.py
@@ -435,7 +435,7 @@ class Client(Clientbase):
         mask_values = read_data(f'save/mask_values_{self.client_id}_{self.is_malicious}.txt')
         mask_values = sorted(mask_values, key=lambda x: float(x[2]))
         print(f'{self.is_malicious} mask_values:{mask_values[0]} - {mask_values[10]}')
-        prune_by_threshold(self.local_model, mask_values, pruning_max=0.5, pruning_step=0.001)
+        prune_by_threshold(self.local_model, mask_values, pruning_max=0.9, pruning_step=0.001)
 
     def handcraft(self, task):
         self.handcraft_rnd = self.handcraft_rnd + 1
