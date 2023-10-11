ab5ad87e53458e6e3423747a7d3ec94d0906214a
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-10 20:54:02 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Bases.py

--- Bases.py
+++ Bases.py
@@ -128,7 +128,7 @@ class FederatedBackdoorExperiment:
             from prune_neuron_cifar import read_data, prune_by_threshold
             mask_values = read_data(f'save/mask_values_{chosen_ids[0]}_False.txt')
             mask_values = sorted(mask_values, key=lambda x: float(x[2]))
-            prune_by_threshold(self.local_model, mask_values, pruning_max=0.95, pruning_step=0.05)
+            prune_by_threshold(self.server.global_model, mask_values, pruning_max=0.95, pruning_step=0.05)
             fl_report.record_round_vars(self.test(epoch, backdoor=False))
             fl_report.record_round_vars(self.test(epoch, backdoor=True))
             print('-' * 30)
