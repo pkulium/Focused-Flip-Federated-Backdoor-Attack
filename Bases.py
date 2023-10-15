a5263e808f4bc38dd19c3089705d1d003c519a2f
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-15 11:29:25 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Bases.py
    	modified:   Params.py

--- Bases.py
+++ Bases.py
@@ -42,7 +42,7 @@ class FederatedBackdoorExperiment:
         base_model = self.task.build_model()
 
         # base_model.load_state_dict(torch.load(f'/work/LAS/wzhang-lab/mingl/code/client_defense/save/{args.model}_{args.backdoor}_{args.defense}.pth'))
-        base_model = replace_bn_with_noisy_bn(base_model)
+        # base_model = replace_bn_with_noisy_bn(base_model)
         base_optimizer = self.task.build_optimizer(base_model)
         splited_dataset = self.task.sample_dirichlet_train_data(params.n_clients)
         server_sample_ids = splited_dataset[params.n_clients]
