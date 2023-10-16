99eb02124f0efecdb72dcc319fb01494c65ba52e
pkulium <54947136+pkulium@users.noreply.github.com>
2023-10-16 10:52:00 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Client.py

--- Client.py
+++ Client.py
@@ -160,7 +160,7 @@ class Client(Clientbase):
 
         self.local_model = replace_bn_with_noisy_bn(self.local_model)
         self.local_model = self.local_model.to(self.device)
-        self.local_model.mask_lr = 0.001
+        self.local_model.mask_lr = 0.01
         self.local_model.anp_eps = 0.4
         self.local_model.anp_steps = 1
         self.local_model.anp_alpha = 0.2
@@ -426,7 +426,7 @@ class Client(Clientbase):
         mask_optimizer = torch.optim.SGD(mask_params, lr=self.local_model.mask_lr, momentum=0.9)
         noise_params = [v for n, v in parameters if "neuron_noise" in n]
         noise_optimizer = torch.optim.SGD(noise_params, lr=self.local_model.anp_eps / self.local_model.anp_steps)
-        for epoch in range(100):
+        for epoch in range(20):
             train_loss, train_acc = mask_train(model=self, criterion=criterion, data_loader=self.server_train_loader,
                                            mask_opt=mask_optimizer, noise_opt=noise_optimizer)
             print(f'epoch:{epoch} train_loss:{train_loss} train_acc:{train_acc}')
